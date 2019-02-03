45054cb6cbee21242e0b01b99402add23d569e04
Ewout Jaspers <egjaspers@gmail.com>
2019-02-03 15:46:16 +0000

    changed height and margin's to fit within screen

--- scripts/components/Charts.js
+++ scripts/components/Charts.js
@@ -8,7 +8,7 @@ class Charts extends Component {
         <div id={id}>
           {(id === 0) ? "" : (<hr />)}
         {console.log(['hallo', country])}
-        <DrawChart key={country.labels.value} code={country.labels.value} data={country.data} country={country.labels.label} title={country.labels.label} remove={this.props.remove} width={this.props.width} /> 
+        <DrawChart key={country.labels.value} code={country.labels.value} data={country.data} country={country.labels.label} title={country.labels.label} remove={this.props.remove} width={this.props.width} height={this.props.height} /> 
         
         </div>
   )
